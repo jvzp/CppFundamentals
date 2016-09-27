ed8b2b81f1580c5a07f3f4d1f3eb80c8e39dd3bc
jvanzurpele <jonas.vanzurpele@altran.com>
2016-09-27 09:01:29 +0200

    Exercises on classes

---
+++ Classes/Ex1/main.cpp
@@ -0,0 +1,12 @@
+#include "complex.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	Complex a(4,8);
+	//cout << "The number a is: "  << a.getReal() << "+" << a.getImag() << "j" << endl;
+	cout << "The number a is: " << a << endl;
+
+}
